4020dc46ac682ff732809eecaf335f4912c910b7
Brittany Boughter <brittany.wood0308@yahoo.com>
2023-03-15 20:19:35 -0700

    question variables created in JavaScript

---
+++ script.js
@@ -0,0 +1,49 @@
+var questions = [{
+    question: "What does HTML stand for?",
+    a: "1. Hyper Text Markup Language",
+    b: "2. Hyper Tool Markup List",
+    c: "3. Hungry Total Meat Lover",
+    d: "4. Hyper Text Manager Language",
+    correct: "1. Hyper Text Markup Language",
+}, 
+{
+    question: "Choose the correct HTML element to define important text.",
+    a: "1. <important>",
+    b: "2. <massive>",
+    c: "3. <strong>",
+    d: "4. <hulk>",
+    correct: "3. <strong>",
+}
+{
+    question: "The *[asterik symbol] is called",
+    a: "1. All Selector",
+    b: "2. Universal Selector",
+    c: "3. General Selector",
+    d: "4. Star",
+    correct: "2. Universal Selector",  
+}
+{
+    question: "Which code removes the underline from links?",
+    a: "1. text-decoration: go away",
+    b: "2. text-decoration: none",
+    c: "3. text-decoration: hide",
+    d: "4. text-underline: none",
+    correct: "2. text-decoration: none",
+}
+{
+    question: "How do you write 'Hello World!' in an alert box?",
+    a: "1. msg('Hello World!')",
+    b: "2. alertBox('Hello World!')",
+    c: "3. noticeMe('Hello World!')",
+    d: "4. alert('Hello World!')",
+    correct: "4. alert('Hello World!')",
+}
+{
+    question: "How do you declare a JavaScript variable?",
+    a: "1. var carName",
+    b: "2. v carName",
+    c: "3. variable carName",
+    d: "4. create carName",
+    correct: "1. var carName",
+}
+];
